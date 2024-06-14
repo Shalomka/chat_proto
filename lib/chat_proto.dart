665552dba0438ceca1c4c353c970acbafa74bb91
Robertas Skiauteris <Shalomka@users.noreply.github.com>
2024-06-14 08:24:37 +0300

    added exports

--- lib/chat_proto.dart
+++ lib/chat_proto.dart
@@ -8,3 +8,7 @@ export 'generated/chat.pbgrpc.dart';
 
 // export timestamp
 export 'generated/google/protobuf/timestamp.pb.dart';
+
+// export grpc and proto libs
+export 'package:grpc/grpc.dart';
+export 'package:protobuf/protobuf.dart';
