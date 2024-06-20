0f3037f6577c114c5738bf4fbddfc8846aa7e999
Robertas Skiauteris <Shalomka@users.noreply.github.com>
2024-06-20 08:05:27 +0300

    removed cache service

--- lib/chat_proto.dart
+++ lib/chat_proto.dart
@@ -13,13 +13,13 @@ export 'generated/google/protobuf/timestamp.pb.dart';
 export 'package:grpc/grpc.dart';
 export 'package:protobuf/protobuf.dart';
 
-// export objectbox models
-export 'cache/models/cached_models.dart';
+// // export objectbox models
+// export 'cache/models/cached_models.dart';
 
-// export objectbox services
-export 'cache/services/cache_repo.dart';
-export 'cache/services/channel_cache_repo.dart';
-export 'cache/services/message_cache_repo.dart';
+// // export objectbox services
+// export 'cache/services/cache_repo.dart';
+// export 'cache/services/channel_cache_repo.dart';
+// export 'cache/services/message_cache_repo.dart';
 
-// export objectbox
-export 'package:objectbox/objectbox.dart';
+// // export objectbox
+// export 'package:objectbox/objectbox.dart';
